bd5e12e9eca269dd7f3db8fafcea6c940237afc9
AlexCamer <alexandru.camer@gmail.com>
2020-08-06 17:50:49 -0400

    peanuts

--- code/View.h
+++ code/View.h
@@ -8,13 +8,12 @@
 
 class View : public Observer {
   private:
-    int pp;
     Game* game;
     std::string view;
-    map<>
+    std::map<std::string, int> positions;
   public:
     View(Game* game);
-    virtual void update(Subject* whoNotified) override;
+    virtual void update(Subject* whoUpdated) override;
     virtual void update(std::string message) override;
     void display();
     std::string getCommand();
