4a08ce07539ec6d56515b2174d100b4fa9ca7731
John Gui <JohnGui@MacBook-Pro-2.local>
2020-08-07 19:25:23 -0400

    a small bug fixed

--- code/DCTimsLine.h
+++ code/DCTimsLine.h
@@ -8,7 +8,7 @@ class DCTimsLine: public NonProperty{
  public:
     DCTimsLine();
     void land(std::shared_ptr<Player> p) override;
-    void applyEffect(std::shared_ptr<Player> p) override;
+    void applyEffect(std::shared_ptr<Player> p);
 
 }; 
 
