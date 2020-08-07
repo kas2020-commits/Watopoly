cebba3cb397bc960f988eb00f6fdb1dc28a8a3c3
John Gui <JohnGui@MacBook-Pro-2.local>
2020-08-07 11:20:09 -0400

    John changed the DC tims Line

--- code/DCTimsLine.cc
+++ code/DCTimsLine.cc
@@ -1,21 +1,61 @@
 #include "DCTimsLine.h"
 #include <string>
+#include <iostream>
 DCTimsLine::DCTimsLine(): NonProperty{DC_TIMS_LINE} {}
 
 void DCTimsLine::land(Player* p){
-    //
 }
 
 
 void DCTimsLine::applyEffect(Player* p){
-    //double dice
-    //50 bucks
-    //use roll up the rims
-    int dice1 = rolldice();
-    int dice2 = rolldice();
-    int dicecount = 0;
-    if (dicecount != 3)
-    if (dice1 != dice2){
-        dicecount += 1;
+    int dice1, dice2, counter;
+    char temp;
+    counter = 0;
+    while (counter < 3){
+        updateObservers("Please choose which action you want to do in the DC Tims Line");
+        updateObservers("A for roll dice, B for pay $50, C for use a roll up the rim cup ");
+        // interaction right here
+        std::cin >> temp;
+        if(temp == 'A'){
+            dice1 = rolldice();
+            dice2 = rolldice();
+            if(dice1 != dice2){
+                updateObservers("Not same, try again");
+                counter += 1;
+            } else {
+                updateObservers("Same number, congrats you may leave now.");
+                break;
+            }
+        } else if (temp == 'B'){
+            p->withdraw(50);
+            updateObservers("Paid 50 dollars for coffee");
+            break;
+        } else if (temp == 'C'){
+            p->changeTimsCups(-1);
+            updateObservers("Use a roll up the rim up!");
+            break;
+        } else {
+            updateObservers("invalid input");
+        }
+        
     }
+    if (counter == 3){
+        updateObservers("You can only pay or use roll up the rim now");
+        updateObservers("A for pay 50 dollars, B for use a roll up the rim cup!");
+        std::cin >> temp;
+        while (true){
+            if (temp == 'A'){
+                p->withdraw(50);
+                updateObservers("Paid 50 dollars for coffee");
+                break;
+            } else if (temp == 'B'){
+                p->changeTimsCups(-1);
+                updateObservers("Use a roll up the rim up!");
+                break;
+            } else {
+                updateObservers("invalid input, try again!");
+            }
+        }
+    }
+
 }
--- code/NonProperty.h
+++ code/NonProperty.h
@@ -2,7 +2,6 @@
 #define NONPROPERTY_H
 #include "Tile.h"
 #include "Player.h"
-#include "util.h"
 
 
 class NonProperty : public Tile{
