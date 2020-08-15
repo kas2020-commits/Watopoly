65eca3bba535e21f129e257e498dd4a03b9f5af4
AlexCamer <alexandru.camer@gmail.com>
2020-08-15 13:44:52 -0400

    fixed residence.cc

--- code/Residence.cc
+++ code/Residence.cc
@@ -8,24 +8,19 @@ Residence::Residence(std::string name, int purchaseCost):
 
 //
 void Residence::applyFee(Player*  p){
-    if (hasOwner()) {
-        int payment;
-        if(p->getResCount() == 1) payment = 25;
-        else if(p->getResCount() == 2) payment = 50;
-        else if(p->getResCount() == 3) payment = 100;
-        else if(p->getResCount() == 4) payment = 200;
-        // else throw exception possibly?
-        try { p->withdraw(payment); }
-        catch (GameException&) { throw Debt{p, owner, payment}; }
-        owner->deposit(payment);
-        std::ostringstream ss{"Payed $"};
-        ss << payment << " for rent to land lord: \"";
-        ss << owner->getName() << "\"";
-        updateObservers(ss.str());
-    }
-    else {
-        // throw buy option
-    }
+    int payment;
+    if(p->getResCount() == 1) payment = 25;
+    else if(p->getResCount() == 2) payment = 50;
+    else if(p->getResCount() == 3) payment = 100;
+    else if(p->getResCount() == 4) payment = 200;
+    // else throw exception possibly?
+    try { p->withdraw(payment); }
+    catch (GameException&) { throw Debt{p, owner, payment}; }
+    owner->deposit(payment);
+    std::ostringstream ss{"Payed $"};
+    ss << payment << " for rent to land lord: \"";
+    ss << owner->getName() << "\"";
+    updateObservers(ss.str());
 }
 
 //
