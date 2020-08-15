fe575b4f9ebc829b4081c504abf5ed8aa54ad92d
AlexCamer <alexandru.camer@gmail.com>
2020-08-15 18:35:14 -0400

    reee

--- code/Auction.d
+++
@@ -1,3 +0,0 @@
-Auction.o: Auction.cc Auction.h LogicException.h Player.h Subject.h \
- Observer.h BoardIterator.h util.h Tile.h Roll.h PlayerException.h \
- GameException.h Property.h
--- code/DCTimsLine.d
+++
@@ -1,3 +0,0 @@
-DCTimsLine.o: DCTimsLine.cc DCTimsLine.h LogicException.h Tile.h \
- Subject.h Observer.h util.h Player.h BoardIterator.h Roll.h \
- PlayerException.h GameException.h
