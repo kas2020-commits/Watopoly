982665e1e00c91b2aca45284db83d6dcc5593e7d
kas2020-commits <kas2020@protonmail.com>
2020-08-15 18:33:52 -0400

    increased main's capacity

--- code/main.cc
+++ code/main.cc
@@ -4,17 +4,46 @@
 #include "Controller.h"
 #include "Game.h"
 #include "View.h"
+#include <iostream>
 
 //
 int main(int argc, char* argv[]) {
 	Game game{};
 	View view{&game};
-	Controller controller{&game, &view};
-	if (argc == 2) {
-		IO loader;
-		loader.load(std::string {argv[1]}, &game, &view);
-		controller.run(false);
+	bool isTesting = false;
+	bool fromSave = false;
+	std::string file;
+	IO loader;
+	std::string optionOne, optionTwo;
+	const std::string TESTING {"-testing"};
+
+	if (argc > 1) optionOne = argv[1];
+	if (argc > 2) optionTwo = argv[2];
+
+	if ((optionOne.compare(TESTING) == 0 || optionTwo.compare(TESTING) == 0)
+			&& !isTesting) {
+		std::cout << "Aids" << std::endl;
+		isTesting = true;
 	}
-	else if (argc == 1) controller.run();
+
+	if (!fromSave && optionOne.compare(TESTING) == 0) {
+		std::cout << "Cancer2" << std::endl;
+		file = optionTwo;
+		fromSave = true;
+	}
+	if (!fromSave && optionTwo.compare(TESTING) == 0) {
+		std::cout << "Cancer1" << std::endl;
+		file = optionOne;
+		fromSave = true;
+	}
+
+	// construct the controller with the correct parameters
+	Controller controller{&game, &view, isTesting};
+
+	// if from a savefile, load it
+	if (fromSave) loader.load(file, &game, &view);
+
+	// start program
+	controller.run(!fromSave);
 }
 
