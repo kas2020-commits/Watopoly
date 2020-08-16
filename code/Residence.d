2f9e6bd71688fd223772504453b1170a4be97320
AlexCamer <alexandru.camer@gmail.com>
2020-08-15 21:30:06 -0400

    rewwr

--- code/Residence.d
+++
@@ -1,3 +0,0 @@
-Residence.o: Residence.cc Residence.h Property.h LogicException.h \
- Player.h Subject.h Observer.h BoardIterator.h util.h Tile.h Roll.h \
- PlayerException.h GameException.h Debt.h
--- code/Tuition.d
+++
@@ -1,3 +0,0 @@
-Tuition.o: Tuition.cc Tuition.h LogicException.h Tile.h Subject.h \
- Observer.h util.h Player.h BoardIterator.h Roll.h PlayerException.h \
- GameException.h
